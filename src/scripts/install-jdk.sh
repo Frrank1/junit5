68d801aeb34e9964a77d622b0ab3c267151954e1
Christian Stein <sormuras@gmail.com>
2017-11-06 23:44:34 +0100

    Upgrade to Mockito 2.11.7

--- src/scripts/install-jdk.sh
+++ src/scripts/install-jdk.sh
@@ -10,5 +10,4 @@ wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/binari
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
-export _JAVA_OPTIONS="--permit-illegal-access -Xmx2048m -Xms512m"
 cd -
